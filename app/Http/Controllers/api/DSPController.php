ba171bc711497a48f2428c0f97033abf4d8167cb
Quan <kubim298@gmail.com>
2019-09-13 11:27:21 +0700

    update API for DSP

--- app/Http/Controllers/api/DSPController.php
+++ app/Http/Controllers/api/DSPController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\api;
 
+use App\Models\AdDetail;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -18,11 +19,23 @@ class DSPController extends Controller
             ->join('std_bidding_methods','ads_detail.std_bidding_method_id','=','std_bidding_methods.id')
             ->where('period_from','<=',$now)
             ->where('period_to','>=',$now)
+            ->where('status','!=',0)
             ->select('ads.*','ads_detail.*','creative_types.name as creative_type','std_bidding_methods.name as bidding_method')
             ->get();
         return $ads;
     }
     public function result(Request $request){
+        $results = $request->toArray();
+        foreach ($results as $result){
+             if($result["status"]=="won"){
+                 $adDetail = AdDetail::find($result["id"]);
+                 $currentBudget = $adDetail->ads_period_budget;
+                 $price_won = $result["price_won"];
+                 $adDetail->ads_period_budget = $currentBudget - $price_won;
+                 $adDetail->save();
+                 break;
+             }
+        }
         return response()->json([
             'status' => 200,
             'message' => 'OK',
